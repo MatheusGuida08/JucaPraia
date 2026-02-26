e54bd01fce44592c566fa3001ff1c15586d5d88a
MatheusGuida08 <matheusguida2010@hotmail.com>
2026-02-25 21:57:26 -0300

    Entrada e consultas no banco

--- Consulta.sql
+++ Consulta.sql
@@ -54,6 +54,10 @@ ADD nomeLogradouro VARCHAR(50) NOT NULL,
 ADD numero VARCHAR(6) NOT NULL, 
 ADD complemento VARCHAR(30);
 
+
+
+
+
 INSERT INTO cliente 
 (nomeCliente, cpf, email, cidade, estado, tipoLogradouro, nomeLogradouro, numero, complemento)
 VALUES
@@ -75,8 +79,6 @@ VALUES
 
 ('Mickey', '9999999999908', 'michey@hotmail.com', 'Recife', 'PE', 'Rua', 'das ostras', '108', 'apto 03');
 
-
-SELECT * FROM cliente
 
 ALTER TABLE cliente
 ALTER COLUMN email VARCHAR(100) NULL;
@@ -126,7 +128,6 @@ VALUES
 
 ('Chico Bento', '34567890123', '13965432109');
 
-select * FROM funcionario
 
 INSERT INTO Equipamento (nomeEquipamento, qtd, valorHora)
 VALUES
@@ -136,9 +137,294 @@ VALUES
 ('Guarda Sol G', 60, 3.00),
 ('Mesinha', 30, 1.50)
 
+INSERT INTO aluguel 
+(idCliente, idFuncionario, dataHoraRetirada, valorAPagar, valorPago, pago)
+VALUES
+(20, 1, '2024-12-08 08:00:00', 2.00, 0, 0)
+
+UPDATE Equipamento
+SET qtd = qtd - 1
+WHERE nomeEquipamento='Cadeira 02 posições';
+
+INSERT INTO aluguel 
+(idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(18, 3, '2024-12-10 00:00:00', 0)
+
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(4, 2, 3.00, 3.50, 1)
+
+UPDATE Equipamento
+SET qtd = qtd - 1
+WHERE idEquipamento=4;
+
+UPDATE Equipamento
+SET qtd = qtd - 2
+WHERE idEquipamento=2;
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(1, 1, 2.00, 2.00, 1)
+
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(2, 2, 7.00, 3.50, 2)
+
+INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(10, 1, '2024-12-24 00:00:00', 0)
+/*Cebolinha alugou pro Donald*/
+
+INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(11, 1, '2024-12-24 00:00:00', 0)
+/*Cebolinha alugou pra Margarida*/
+
+INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(13, 1, '2024-12-24 00:00:00', 0)
+/*Cebolinha alugou pro Huguinho*/
+
+UPDATE Equipamento
+SET qtd = qtd - 3
+WHERE idEquipamento=3;
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(3, 3, 2.00, 2.00, 1)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(3, 4, 2.00, 2.00, 1)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(3, 5, 2.00, 2.00, 1)
+
+UPDATE aluguelequipamento 
+SET idAluguel =5
+WHERE idAluguelEquipamento=6
+
+/* parte 9 */
+
+/*Chico aluga para o aladin gera o idAluguel 6)*/
+INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(22, 3, '2024-12-28 15:00:00',0)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(2, 6, 7.00, 3.50, 2)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(4, 6, 3.00, 3.00, 1)
+
+UPDATE equipamento 
+SET qtd = qtd -2
+WHERE idEquipamento = 2
+
+UPDATE equipamento 
+SET qtd = qtd -1
+WHERE idEquipamento = 4
+
+
+/*Chico aluga para a Cinderela gera o idAluguel 7)*/
+INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(23, 3, '2024-12-28 16:00:00', 0)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(2, 7, 7.00, 3.50, 2)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(4, 7, 3.00, 3.00, 1)
+
+UPDATE equipamento 
+SET qtd = qtd -2
+WHERE idEquipamento = 2
+
+UPDATE equipamento 
+SET qtd = qtd -1
+WHERE idEquipamento = 4
+
+/*Chico aluga para a Mulan gera o idAluguel 8)*/
+INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(24, 3, '2024-12-28 17:00:00', 0)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(2, 8, 7.00, 3.50, 2)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(4, 8, 3.00, 3.00, 1)
+
+UPDATE equipamento 
+SET qtd = qtd -2
+WHERE idEquipamento = 2
+
+UPDATE equipamento 
+SET qtd = qtd -1
+WHERE idEquipamento = 4
+
+/*Chico aluga para a Zezinho gera o idAluguel 9)*/
+INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(15, 3, '2024-12-28 13:00:00', 0)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(2, 9, 7.00, 3.50, 2)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(4, 9, 3.00, 3.00, 1)
+
+UPDATE equipamento 
+SET qtd = qtd -2
+WHERE idEquipamento = 2
+
+UPDATE equipamento 
+SET qtd = qtd -1
+WHERE idEquipamento = 4
+
+/*Chico aluga para o Asnésio gera o idAluguel 10)*/
+INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(26, 3, '2024-12-28 13:35:00', 0)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(2, 10, 7.00, 3.50, 2)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(4, 10, 3.00, 3.00, 1)
+
+UPDATE equipamento 
+SET qtd = qtd -2
+WHERE idEquipamento = 2
+
+UPDATE equipamento 
+SET qtd = qtd -1
+WHERE idEquipamento = 4
+
+/*Chico aluga para a Moana gera o idAluguel 11)*/
+INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada, pago)
+VALUES
+(25, 3, '2024-12-28 13:35:00', 0)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(2, 11, 7.00, 3.50, 2)
+
+INSERT INTO AluguelEquipamento
+(idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+VALUES
+(4, 11, 3.00, 3.00, 1)
+
+UPDATE equipamento 
+SET qtd = qtd -2
+WHERE idEquipamento = 2
+
+UPDATE equipamento 
+SET qtd = qtd -1
+WHERE idEquipamento = 4
+
+
 SELECT * FROM equipamento
 
 SELECT * FROM aluguel
 
+SELECT * FROM aluguelequipamento
 
+SELECT * FROM funcionario
+
+SELECT * FROM cliente
+
+/* Parte 10*/
+
+SELECT nomeCliente, email FROM cliente
+
+/*Parte 11*/
+
+SELECT nomeFuncionario, celular FROM funcionario
+
+/*Parte 12*/
+
+SELECT * FROM aluguel ORDER BY  dataHoraRetirada
+
+/*Parte 13*/
+
+SELECT nomeCliente, cidade, estado 
+FROM cliente 
+WHERE cidade IN ('Santos', 'São Vicente', 'Praia Grande', 'Cubatão') 
+ORDER BY nomeCliente
+
+/*Parte 14*/
+
+SELECT nomeEquipamento, qtd FROM equipamento
+
+/*Parte 15*/
+
+SELECT nomeCliente, cidade, estado 
+FROM cliente 
+WHERE complemento is NULL 
+ORDER BY NomeCliente
+
+/*Parte 16*/
+
+SELECT nomeCliente, estado 
+FROM cliente 
+WHERE estado <> 'SP'
+
+/*Parte 17*/
+
+SELECT nomeCliente 
+FROM cliente
+WHERE nomeCliente LIKE 'A%'
+
+/*Parte 18*/
+
+SELECT nomeCliente
+FROM cliente
+WHERE nomeCliente LIKE 'M%' AND estado = 'PE'
+
+/*Parte 19 */
+
+SELECT nomeEquipamento, qtd
+FROM equipamento
+WHERE nomeEquipamento LIKE 'CADEIRA%'
+ORDER BY qtd DESC
+
+/*Parte 20*/
+
+SELECT * 
+FROM aluguel
+WHERE dataHoraRetirada BETWEEN '2024-12-25 00:00:00' AND '2024-12-31 23:59:59'
+ORDER BY dataHoraRetirada ASC
 
